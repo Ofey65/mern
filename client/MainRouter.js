e745b140387128d35b71abf35a43ae4ee7ef26b0
ofey <einswinston@gmail.com>
2020-11-14 20:36:32 -0600

    Navbar added!

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 
 import Home from './core/Home'
+import Menu from './core/Menu'
 import Users from './user/Users'
 import Signup from './user/Signup'
 import Signin from './auth/Signin'
@@ -12,6 +13,7 @@ import PrivateRoute from './auth/PrivateRoute'
 const MainRouter = () => {
     return (
         <div>
+            <Menu/>
             <Switch>
                 <Route exact path="/" component={Home}/>
                 <Route path="/users" component={Users}/>
